e2bceb76ac31bac97c1d345d27f333cbc513efba
André <andrsilvestre289@gmail.com>
2013-11-13 22:12:01 +0000

    commit inicial

---
+++ carluguer cpp.cpp
@@ -0,0 +1,61 @@
+do
+  {
+   printf ("menu inicial");
+   printf ("1- Cliente");
+   printf ("2- Automóveis");
+   printf ("3­ Aluguer");
+   printf ("4- Pesquisas");
+   printf ("5- Estatistícas"); 
+   scanf ("menu inicial");
+   while menu inicial=1
+   {
+   printf ("menu cliente");
+   printf ("1- listar clientes");
+   printf ("2- Novo cliente");
+   printf ("3­ Alterar cliente");
+   printf ("4- Apagar cliente");
+   printf ("5- Voltar ao menu inicial"); 
+   scanf ("menu cliente");
+   while menu cliente=1;
+   }
+   {
+   while menu inicial=2;
+   printf ("menu do automóvel");
+   printf ("1- listar automóvel");
+   printf ("2- Novo automóvel");
+   printf ("3­ Alterar automóvel");
+   printf ("4- Apagar automóvel");
+   printf ("5- Voltar ao menu inicial"); 
+   scanf (menu cliente);
+   }
+   printf ("menu do aluguer");
+   printf ("1- listar aluguer");
+   printf ("2- novo aluguer");
+   printf ("Voltar ao menu inicial");
+   {
+   printf ("menu das pesquisas");
+   printf ("1- pesquisa por cliente");
+   printf ("2- pesquisa por automóvel");
+   printf ("3- pesquisa do aluguer por data");
+   printf ("4- pesquisa do aluguer por carro");
+   printf ("5- pesquisa do aluguer por cliente");
+   }
+   printf  ("menu das estatísticas");
+   printf  ("1- veículo com mais alugueres");
+   printf  ("2- veículo com mais Km's");
+   printf  ("3- veículo com maior número de acidentes");
+   printf  ("4- média das distancias percorridas");
+   printf  ("5- maior viagem em termos de distancias");
+   printf  ("6- Número médio de dias de aluguer por veículo");
+   printf  ("7- Número de dias que cada veículo esteve parado");
+   printf  ("8- Cliente que fez mais alugueres");
+   printf  ("9- Cliente que percorreu maior distância"); 
+   printf  ("10- Cliente com maior número de acidentes");
+   printf  ("11- Voltar ao menu principal");
+   
+   until  
+   
+   
+    
+  }
+  
